edbca8f364d097c7bb112139b4d2bccb651931ab
lu1-cloud <santiago.urayama@virginiogomez.cl>
2025-05-16 17:07:35 +0000

    2

--- ejercicio2.py
+++ ejercicio2.py
@@ -10,3 +10,25 @@
 # 5. Todas las notas deben estar entre 1.0 y 7.0.
 
 # 👇 Aquí comienza tu código
+# Paso 1: Solicitar cuántas notas ingresará el usuario
+cantidad_notas = int(input("¿Cuántas notas vas a ingresar? "))
+
+notas = []
+
+for i in range(cantidad_notas):
+    while True:
+       
+        nota = float(input(f"Ingrese la nota {i+1}: "))
+        if 1.0 <= nota <= 7.0:
+            notas.append(nota)
+            break
+        else:
+            print("La nota debe estar entre 1.0 y 7.0. Intenta nuevamente.")
+
+promedio = sum(notas) / len(notas)
+promedio = round(promedio, 2)
+
+if promedio >= 4.0:
+    print(f"Tu promedio es {promedio}. ¡Aprobaste!")
+else:
+    print(f"Tu promedio es {promedio}. No aprobaste.")
