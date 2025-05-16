5aad4bff1b88b5e60727f133d8a62ac351cf4a97
lu1-cloud <santiago.urayama@virginiogomez.cl>
2025-05-16 13:31:14 +0000

    hola1

--- ejercicio1.py
+++ ejercicio1.py
@@ -9,3 +9,12 @@
 # 4. Luego, solicita al usuario un número y llama a la función con ese valor
 
 # 👇 Aquí comienza tu código
+def mostrar_tabla(n):
+
+    for i in range(1, 11):
+        print(f"{n} x {i} = {n * i}")
+
+numero = int(input("Introduce un número para ver su tabla de multiplicar: "))
+
+
+mostrar_tabla(numero)
